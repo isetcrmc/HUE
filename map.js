0ba4c8c563224feb6f7e2bd18785655145bb61bc
iset <108780971+ttthhhhh@users.noreply.github.com>
2025-06-12 09:09:06 +0700

    Add files via upload

---
+++ map.js
@@ -0,0 +1,90 @@
+const map = L.map('map').setView([16.4637, 107.5909], 11);
+
+// Base map Google
+const baseLayers = {
+  "Roadmap": L.tileLayer('https://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}', { attribution: "Google Roadmap" }),
+  "Satellite": L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', { attribution: "Google Satellite" })
+};
+baseLayers["Roadmap"].addTo(map);
+
+// Overlay groups
+const overlays = {
+  "Vết lũ": {},
+  "Station": {}
+};
+
+// Add flood data by year
+function addFloodLayer(year, color) {
+  fetch("Flood_trace_all.geojson").then(res => res.json()).then(data => {
+    const layer = L.geoJSON(data, {
+      filter: f => f.properties[`VL${year}`],
+      pointToLayer: (f, latlng) => L.circleMarker(latlng, {
+        radius: map.getZoom() / 2,
+        fillColor: color,
+        color: "#000",
+        weight: 1,
+        fillOpacity: 0.85
+      }),
+      onEachFeature: (f, l) => {
+        let p = f.properties;
+        let popup = `<b>${p.Name}</b><br><b>Code:</b> ${p.Code}<br><b>Vị trí:</b> ${p.Commune}, ${p.District}<br><b>Tọa độ:</b> ${p.X}, ${p.Y}`;
+        ['2020', '2022', '2023'].forEach(y => {
+          let val = p[`T10_${y}`] || p[`T11_${y}`] || p[`T10.${y}`] || p[`T11.${y}`];
+          if (val && !isNaN(parseFloat(val))) {
+            popup += `<br><b>Độ sâu ${y}:</b> ${parseFloat(val).toFixed(2)} m`;
+          }
+        });
+        l.bindPopup(popup);
+      }
+    });
+    overlays["Vết lũ"][`Vết lũ ${year}`] = layer;
+    layer.addTo(map);
+  });
+}
+addFloodLayer('2020', 'orange');
+addFloodLayer('2022', 'yellow');
+addFloodLayer('2023', 'green');
+
+// Add station layers
+fetch("Station.geojson").then(res => res.json()).then(data => {
+  const types = ["Tháp báo lũ", "Trạm đo H tự động", "Tháp cảnh báo ngập"];
+  const colors = {
+    "Tháp báo lũ": "red",
+    "Trạm đo H tự động": "blue",
+    "Tháp cảnh báo ngập": "cadetblue"
+  };
+
+  types.forEach(type => {
+    const layer = L.geoJSON(data, {
+      filter: f => f.properties.Type === type,
+      pointToLayer: (f, latlng) => L.marker(latlng, {
+        icon: L.AwesomeMarkers.icon({
+          icon: 'info-sign',
+          prefix: 'glyphicon',
+          markerColor: colors[type]
+        })
+      }),
+      onEachFeature: (f, l) => {
+        const p = f.properties;
+        const popup = `<b>${p.Name2 || p.Name}</b><br><b>Loại:</b> ${p.Type}<br><b>Vị trí:</b> ${p.Name}<br><b>Tọa độ:</b> ${p.X}, ${p.Y}`;
+        l.bindPopup(popup);
+      }
+    });
+    overlays["Station"][type] = layer;
+    layer.addTo(map);
+  });
+});
+
+// Layer control
+L.control.groupedLayers(baseLayers, overlays, {
+  collapsed: false,
+  position: "topleft"
+}).addTo(map);
+
+// Resize circle on zoom
+map.on("zoomend", () => {
+  const z = map.getZoom() / 2;
+  Object.values(overlays["Vết lũ"]).forEach(layer => {
+    if (layer.setStyle) layer.setStyle({ radius: z });
+  });
+});
